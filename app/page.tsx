b71ba95873b2b8f664c500780f85e5500bd4f65d
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-15 11:56:59 -0400

    added mapping page for players

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
+"use client";
+import { PlayerStats } from "@/utils/types";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [items, setItems] = useState<PlayerStats[]>([]);
+
   return (
-   <div>
-    <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-   </div>
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Kills</th>
+              <th>Blocks</th>
+              <th>Digs</th>
+              <th>Assists</th>
+              <th>Aces</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(items) &&
+              items.map((player: PlayerStats) => (
+                <tr key={player.id}>
+                  <th>1</th>
+                  <td>g</td>
+                  <td>f</td>
+                  <td>c</td>
+                  <td>c</td>
+                  <td>c</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
