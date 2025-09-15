71f954bcf04d6f1d8c39e3713aa3499e54dcb725
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-15 10:36:40 -0400

    created sample data

---
+++ data/data.tsx
@@ -0,0 +1,135 @@
+const data = {
+  players: [
+    {
+      id: 1,
+      name: "Fatima Bryan",
+      
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Colleen Clark ",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Merin Czerwiec",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "kathrine DeCaro",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Eija Hatcher",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Samantha Hess",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Mckayala Nergrown-Mautro",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Kayla Pecoy",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Caroline Tabb",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Paiyton Trombly",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Payton Verterame",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Lindsey Wiatrowski",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+    {
+      id: 1,
+      name: "Mary Bajakian",
+      kills: "",
+      digs: "",
+      blocks: "",
+      aces: "",
+
+      receptions: "",
+    },
+  ],
+};
